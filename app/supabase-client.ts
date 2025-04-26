c83946ad48a5dca414ee35cc0e78259d44500acb
mahak porwal <porwalmahak784@gmail.com>
2025-04-26 10:50:37 +0530

    first commit

---
+++ app/supabase-client.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase=createClient(
+
+    "https://nnvfkibacljxxhfhrgtb.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5udmZraWJhY2xqeHhoZmhyZ3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NjEzNDAsImV4cCI6MjA2MTEzNzM0MH0.4Bz4nJH4e1YUVVSktFEGhanE_SXRv7RbSBbcBSQpEXI"
+
+);
+
